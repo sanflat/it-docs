bb42114df1657e10c423987bef0bc9859bfe2e5e
sanflat <tradeforce0720@gmail.com>
2022-10-24 10:07:57 +0900

    first commit

--- theme.config.js
+++ theme.config.js
@@ -4,10 +4,7 @@ export default {
   titleSuffix: ' – Nextra',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
-      <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
-      </span>
+      <span className="mr-2 font-extrabold hidden md:inline">IT Docs</span>
     </>
   ),
   head: (
